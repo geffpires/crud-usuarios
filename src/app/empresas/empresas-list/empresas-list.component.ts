ea22552d6763c15fdd81963456d1b93cf4045e19
geffpires <gefferson.pires@dcx.ufpb.br>
2020-03-04 15:35:08 -0300

    add empresas-list

---
+++ src/app/empresas/empresas-list/empresas-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empresas-list',
+  templateUrl: './empresas-list.component.html',
+  styleUrls: ['./empresas-list.component.css']
+})
+export class EmpresasListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/empresas/empresas.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmpresasListComponent } from './empresas-list/empresas-list.component';
+
+
+
+@NgModule({
+  declarations: [EmpresasListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class EmpresasModule { }
